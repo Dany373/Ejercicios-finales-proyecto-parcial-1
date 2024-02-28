7a8ebeab0c8f16cc8c4bc1b628fe5a35b15d23bb
Dany Mateo <dm614305@gmail.com>
2024-02-28 13:23:10 -0600

    Agregar archivos de proyecto.

---
+++ muestra 1/Program.cs
@@ -0,0 +1,142 @@
+﻿// Concatenación de cadenas
+
+
+using System.Net.NetworkInformation;
+
+string saludo = "Hola";
+string nombre = "Dany";
+string mensaje = saludo + ", " + nombre + "!";
+Console.WriteLine(mensaje);
+
+
+//Método Concat
+   string saludo = "Hola";
+string nombre = "Dany";
+string mensaje = String.Concat(saludo, ", ", nombre, "!");
+
+
+//Interpolacion de cadenas 
+string saludo = "Hola";
+string nombre = "Dany";
+string mensaje = $"{saludo}, {nombre}!";
+
+
+
+//Método String.Format
+string saludo = "Hola";
+string nombre = "Dany";
+string mensaje = String.Format("{0}, {1}!", saludo, nombre);
+Console.WriteLine(mensaje);
+
+
+// • Métodos:
+
+//1. Length
+string mensaje = "Hola";
+int longitud = mensaje.Length; //(longitud sería 4).
+Console.WriteLine("La longitud es: " + longitud);
+
+
+
+
+//2. ToUpper()
+string mensaje = "hola";
+string mayusculas = mensaje.ToUpper(); //(mayusculas sería "HOLA").
+Console.WriteLine(mayusculas);
+
+
+//3. ToLower()
+string mensaje = "HOLA"; 
+string minusculas = mensaje.ToLower(); //(minusculas sería "hola").
+Console.WriteLine(minusculas);
+
+//4. IndexOf()
+string mensaje = "Hola mundo";
+int indice = mensaje.IndexOf("m"); //(índice sería 5).
+Console.WriteLine("el indice es:" + indice);
+
+
+//5. LastIndex()
+string mensaje = "Hola mundo";
+int ultimoIndice = mensaje.LastIndexOf("o"); //(últimoIndice sería 9, ya que la última "o" está en la posición 9).
+Console.WriteLine("El ultimo indice es: " + ultimoIndice);
+
+
+//6. Substring()
+
+//Ejemplo 1: 
+string mensaje = "Hola mundo";
+string subcadena = mensaje.Substring(5); //(subcadena sería "mundo", porque comienza en el índice 5).
+Console.WriteLine(subcadena);
+
+//Ejemplo 2:
+string mensaje = "Hola mundo";
+string subcadena = mensaje.Substring(0, 4); //(subcadena sería "Hola", porque comienza en el índice 0 y se obtienen 4 caracteres).
+Console.WriteLine(subcadena);
+
+
+
+
+//7. Replace()
+
+string mensaje = "Hola mundo";
+string nuevoMensaje = mensaje.Replace("mundo", "amigo"); //(nuevoMensaje sería "Hola amigo", porque "mundo" se reemplaza por "amigo").
+Console.WriteLine(nuevoMensaje);
+
+
+//8. Trim()
+
+string mensaje = " Hola mundo ";
+string limpio = mensaje.Trim(); //(limpio sería "Hola mundo", sin espacios al principio ni al final).
+Console.WriteLine(limpio);
+
+
+//9. . StartsWith()
+
+string mensaje = "Hola mundo";
+bool empiezaConHola = mensaje.StartsWith("Hola"); //(empiezaConHola sería true, porque "Hola mundo" comienza con "Hola").
+Console.WriteLine(empiezaConHola);
+
+
+//10. EndsWith()
+
+string mensaje = "Hola mundo";
+bool terminaConMundo = mensaje.EndsWith("mundo"); //(terminaConMundo sería true, porque "Hola mundo" termina con "mundo").
+Console.WriteLine(terminaConMundo);
+
+
+//COMPARACION DE CADENAS 
+string cadena1 = "Hola";
+string cadena2 = "Hola";
+
+bool sonIguales = (cadena1 == cadena2); // true
+bool sonDiferentes = (cadena1 != cadena2); // false
+int resultadoComparacion = cadena1.CompareTo(cadena2); // 0 (son iguales)
+
+Console.WriteLine(sonIguales);
+Console.WriteLine(sonDiferentes);
+Console.WriteLine(resultadoComparacion);    
+
+
+
+
+
+
+//CONCATENACION DE CADENAS
+string cadena1 = "Hola";
+string cadena2 = "mundo";
+string resultado1 = cadena1 + cadena2; // 
+string resultado2 = String.Concat(cadena1, cadena2); 
+
+Console.WriteLine(resultado1);
+Console.WriteLine(resultado2);
+
+
+//FORMATEO DE CADENAS 
+
+string nombre = "Juan";
+int edad = 30;
+
+string mensaje1 = String.Format("Hola, {0}, tienes {1} años.", nombre, edad);
+string mensaje2 = $"Hola, {nombre}, tienes {edad} años.";
+
---
+++ muestra/Program.cs
@@ -0,0 +1,139 @@
+﻿
+
+//CALCULAR FACTORIAL O RAIZ CUADRADA
+
+int numero;
+Console.WriteLine("Introduzca un número entero positivo: ");
+numero = int.Parse(Console.ReadLine());
+int opcion;
+do
+{
+    Console.WriteLine("\nElija una opción:");
+    Console.WriteLine("1. Calcular el factorial del número");
+    Console.WriteLine("2. Calcular la raíz cuadrada del número");
+    Console.WriteLine("3. Salir del programa");
+    opcion = int.Parse(Console.ReadLine());
+
+    switch (opcion)
+    {
+        case 1:
+            Console.WriteLine("El factorial de {0} es {1}", numero, CalcularFactorial(numero));
+            break;
+        case 2:
+            Console.WriteLine("La raíz cuadrada de {0} es {1}", numero, Math.Sqrt(numero));
+            break;
+        case 3:
+            Console.WriteLine("¡Hasta luego!");
+            break;
+        default:
+            Console.WriteLine("Opción no válida. Intente de nuevo.");
+            break;
+    }
+} while (opcion != 3);
+    
+
+    static int CalcularFactorial(int numero)
+{
+    if (numero == 0 || numero == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return numero * CalcularFactorial(numero - 1);
+    }
+}
+
+
+
+//NUMERO Y OPERADOR 
+
+
+Console.WriteLine("Introduzca el primer número: ");
+int num1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Introduzca el segundo número: ");
+int num2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Introduzca el operador (+, -, *, /): ");
+string operador = Console.ReadLine();
+
+if (!ValidarOperador(operador))
+{
+    Console.WriteLine("Operador no válido. Intente de nuevo.");
+    return;
+}
+
+int resultado = 0;
+switch (operador)
+{
+    case "+":
+        resultado = num1 + num2;
+        break;
+    case "-":
+        resultado = num1 - num2;
+        break;
+    case "*":
+        resultado = num1 * num2;
+        break;
+    case "/":
+        if (num2 == 0)
+        {
+            Console.WriteLine("No se puede dividir por 0. Intente de nuevo.");
+            return;
+        }
+        resultado = num1 / num2;
+        break;
+}
+
+Console.WriteLine("El resultado de la operación {0} {1} {2} es {3}", num1, operador, num2, resultado);
+    
+
+    static bool ValidarOperador(string operador)
+{
+    return operador == "+" || operador == "-" || operador == "*" || operador == "/";
+}
+
+
+
+//TABLA DE MULTIPLICAR DE UN NUMERO 
+
+Console.WriteLine("Introduzca un número: ");
+int num = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Tabla de multiplicar del {0}", num);
+for (int i = 1; i <= 10; i++)
+{
+    Console.WriteLine("{0} x {1} = {2}", num, i, num * i);
+}
+
+
+
+
+//ADIVINAR NUMERO SECRETO 
+
+// Generar un número secreto aleatorio
+Random random = new Random();
+int numeroSecreto = random.Next(1, 101);
+
+Console.WriteLine("Introduzca un número entre 1 y 100: ");
+int numeroUsuario = int.Parse(Console.ReadLine());
+
+while (numeroUsuario != numeroSecreto)
+{
+   
+    if (numeroUsuario > numeroSecreto)
+    {
+        Console.WriteLine("El número secreto es menor que {0}", numeroUsuario);
+    }
+    else
+    {
+        Console.WriteLine("El número secreto es mayor que {0}", numeroUsuario);
+    }
+
+    Console.WriteLine("Introduzca otro número: ");
+    numeroUsuario = int.Parse(Console.ReadLine());
+}
+
+Console.WriteLine("¡Felicidades! Has adivinado el número secreto: {0}", numeroSecreto);
+    
